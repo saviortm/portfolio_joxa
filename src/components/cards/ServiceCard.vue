b7aad457831b949e75d291c4980a93b06e69d4d3
saviortm <106533562+saviortm@users.noreply.github.com>
2023-09-04 21:00:47 +0500

    wip service card

--- src/components/cards/ServiceCard.vue
+++ src/components/cards/ServiceCard.vue
@@ -1,35 +1,28 @@
-<script setup>
-import { defineProps } from 'vue'
+<!--<script setup>-->
+<!--import { defineProps } from 'vue'-->
 
-const props = defineProps({
-    title: {
-        type: String,
-        required: true,
-        default: () => '',
-    },
-    subTitle: {
-        type: String,
-        required: true,
-        default: () => '',
-    },
-    img: {
-        type: String,
-        required: true,
-        default: () => '',
-    },
-})
-</script>
+<!--const props = defineProps({-->
+<!--  title: {-->
+<!--    type: String,-->
+<!--    required: true,-->
+<!--    default: () => '',-->
+<!--  },-->
+<!--  subTitle: {-->
+<!--    type: String,-->
+<!--    required: true,-->
+<!--    default: () => '',-->
+<!--  },-->
+<!--  img: {-->
+<!--    type: String,-->
+<!--    required: true,-->
+<!--    default: () => '',-->
+<!--  },-->
+<!--})-->
+<!--</script>-->
 
 <template>
-    <div
-        :style="`background-image: url(${props.img});`"
-        class="bg-no-repeat h-[320px] w-[420px] flex items-center justify-center my-16"
-    >
-        <h1 class="font-semibold text-color-title text-5xl text-shadow">
-            {{ props.title }}
-        </h1>
-        <p class="font-semibold text-color-title text-5xl text-shadow">
-            {{ props.title }}
-        </p>
-    </div>
+  <div
+    class="bg-no-repeat h-[320px] w-[420px] flex items-center justify-center my-16"
+  >
+  </div>
 </template>
--- src/components/layouts/Services.vue
+++ src/components/layouts/Services.vue
@@ -1,14 +1,28 @@
 <script setup>
-import { TitleSection } from '../blocks';
+import {reactive} from "vue";
+import { TitleSection } from '../blocks'
+import { ServiceCard } from '../cards'
 import serviceImg from '../../../public/images/services.png'
 
+
 const name = 'Services'
+
+const data = reactive({
+    serviceData: [
+        {
+            title: '',
+            subTitle: '',
+            img: ''
+        },
+    ]
+})
 </script>
 
 <template>
   <div class="grid-container">
-      <TitleSection :img="serviceImg" :title="name"/>
+    <TitleSection :img="serviceImg" :title="name" />
     <div class="grid grid-cols-3 gap-x-5 gap-y-16">
+      <ServiceCard />
     </div>
   </div>
 </template>
